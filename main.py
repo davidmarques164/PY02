ceebd710c81aa8a75d00adb604fdad46e786424b
davidmarques164 <davidpeterpaulmarques@gmail.com>
2026-07-21 20:38:12 -0300

    Adiciona rotas de autenticação e pedidos, incluindo arquivos .py e .pyc

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-    from fastapi import FastAPI 
-    app = FastAPI()
+from fastapi import FastAPI 
+from passlib.context import CryptContext
+app = FastAPI()
 
-    from auth_routes import auth_router
-    from order_routes import order_router
+from auth_routes import auth_router
+from order_routes import order_router
 
-    app.include_router(auth_router)
-    app.include_router(order_router)
+app.include_router(auth_router)
+app.include_router(order_router)
 
     # uvicorn main:app --reload para rodar
     # endpoints:
